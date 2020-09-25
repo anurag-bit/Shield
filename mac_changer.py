ee5d57ac9bd5afa525ab224cf5556cf08f64be2b
Ravi shankar <singh.ask@outlook.com>
2020-09-25 16:46:18 +0530

    luanch

--- mac_changer.py
+++ mac_changer.py
@@ -16,7 +16,7 @@ def get_arguments():
 def change_mac(interface, new_mac):
 	print("[+] Changing The Mac address for {0} to {1}".format(interface, new_mac))
 
-	subprocess.call(["ifconfig", interface, "down"])
+	subprocess.call([ "ifconfig", interface, "down"])
 	subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
 	subprocess.call(["ifconfig", interface, "up"])
 
